6e9f50083441f6da815213d86c5d47b92612a47f
Balabanova <balabanova_elia@mail.ru>
2024-05-23 19:27:12 +0500

    HW7: number 13: last fix

--- HW7/base_functions.py
+++ HW7/base_functions.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 
 def wait_element_disappearance(driver, element, timeout=5):
@@ -9,4 +9,24 @@ def wait_element_disappearance(driver, element, timeout=5):
         return c
     except TimeoutException as e:
         return False
+
+
+def get_element(driver, locator, timeout=5):
+    try:
+        return WebDriverWait(driver, timeout).until(EC.presence_of_element_located(locator),
+                                                    message=f"Can't find elements by locator {locator}")
+    except TimeoutException as e:
+        return None
+    except NoSuchElementException as e:
+        return None
+
+
+def get_elements(driver, locator, timeout=5):
+    try:
+        return WebDriverWait(driver, timeout).until(EC.presence_of_all_elements_located(locator),
+                                                    message=f"Can't find elements by locator {locator}")
+    except TimeoutException as e:
+        return []
+    except NoSuchElementException as e:
+        return []
 
--- HW7/tests/test_add_delete_from_cart.py
+++ HW7/tests/test_add_delete_from_cart.py
@@ -1,48 +1,42 @@
 from base_functions import wait_element_disappearance
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-import time
+from base_functions import get_elements, get_element
 
 
 def get_items_from_table(driver):
-    return driver.find_elements(By.CSS_SELECTOR, '#order_confirmation-wrapper tbody tr:not(.header) .item')
+    return get_elements(driver, (By.CSS_SELECTOR, '#order_confirmation-wrapper tbody tr:not(.header) .item'))
 
 
 def test_cart(driver):
-    driver.implicitly_wait(3)
-
+    driver.implicitly_wait(0)
     for i in range(3):
         # Переходим на главную страницу
         driver.get("http://localhost/litecart/en/")
         # Переходим в карточку первого из популярных товара
-        driver.find_element(By.CSS_SELECTOR, "#box-most-popular li.product:first-child").click()
-        # Добавляем товар в корзину
-        button = driver.find_element(By.CSS_SELECTOR, 'button[value="Add To Cart"]')
-        button.click()
-        # Получаем элемент корзины, если товаров в ней на 1 больше, чем было
-        g = driver.find_elements(By.XPATH, f'//*[@id="cart"]/a[2]/span[@class="quantity" and contains(text(), "{i+1}")]')
-        # Если количество товара в корзине не соответствует предыдущему условию,
-        # проверяем товар на наличие дополнительных и обязательных к заполнению опций
-        if not g:
-            options = driver.find_element(By.CSS_SELECTOR, '.options select[name="options[Size]"]')
+        get_element(driver, (By.CSS_SELECTOR, "#box-most-popular li.product:first-child")).click()
+
+        button = get_element(driver, (By.CSS_SELECTOR, 'button[value="Add To Cart"]'))
+        # Проверяем товар на наличие дополнительных и обязательных к заполнению опций
+        options = get_element(driver, (By.CSS_SELECTOR, '.options select[name="options[Size]"]'), 1)
+        if options:
             # Устанавливаем значение опции
-            if options:
-                select = Select(options)
-                select.select_by_value("Small")
-            button.click()
-            # Еще раз получаем элемент корзины
-            g = driver.find_elements(By.XPATH, f'//*[@id="cart"]/a[2]/span[@class="quantity" and contains(text(), "{i + 1}")]')
+            select = Select(options)
+            select.select_by_value("Small")
+        # Добавляем товар в корзину
+        button.click()
+        g = get_elements(driver, (By.XPATH, f'//*[@id="cart"]/a[2]/span[@class="quantity" and contains(text(), "{i + 1}")]'))
         # Проверяем, что элемент корзины с верным числом товаров в ней найден
         assert g[0]
 
     # Переходим в карзину
-    driver.find_element(By.CSS_SELECTOR, "#cart").click()
+    get_element(driver, (By.CSS_SELECTOR, "#cart")).click()
     # Находи все элементы, содержащиеся в строках таблицы заказа
     items_count = len(get_items_from_table(driver))
     # Поочередно удаляем товары из корзины и проверяем, что таблица заказа обновилась
     for i in range(0, items_count):
         item = get_items_from_table(driver)[0]
-        b = driver.find_element(By.CSS_SELECTOR, 'button[name="remove_cart_item"]')
+        b = get_element(driver, (By.CSS_SELECTOR, 'button[name="remove_cart_item"]'))
         b.click()
         wait_element_disappearance(driver, b)
         assert wait_element_disappearance(driver, item)
